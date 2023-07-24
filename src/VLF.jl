11db456bcea7cf3a9140348d2b9cedef2ca0b9d7
rydi2626 <rydi2626@colorado.edu>
2023-07-24 16:54:36 -0600

    First push

--- src/VLF.jl
+++ src/VLF.jl
@@ -14,10 +14,7 @@ module VLF
     export plot_data
     export unwrap
 
-    function read_multiple_mat_files(folder_path::AbstractString, 
-        
-
-        include_pattern::AbstractString)
+    function read_multiple_mat_files(folder_path::AbstractString, include_pattern::AbstractString)
         mat_files = filter(f -> isfile(joinpath(folder_path, f)), readdir(folder_path))
         data = []
     
@@ -29,158 +26,189 @@ module VLF
                 include_file = occursin(include_pattern, file)
                 
                 if include_file
-                    mat_contents = MAT.matopen(file_path)
+                    mat_contents = matopen(file_path)
                     push!(data, mat_contents)
                 end
             end
         end
     
-            return data
+        return data
     end
 
-    function read_data(files::Vector) #Get all information needed form each data file, push into array of arrays
+    function read_data(files::Vector,first_date::String,last_date::String) #Get all information needed form each data file, push into array of arrays
 		start_year = Vector{Float64}()
 		start_month = Vector{Float64}()
 		start_day = Vector{Float64}()
 		start_hour = Vector{Float64}()
 		start_minute = Vector{Float64}()
 		start_second = Vector{Float64}()
-		data = []
-		for i in 1:length(files)
-            try
-			    curr_day = read(files[i],"start_day")
-                curr_min = read(files[i],"start_minute")
-                curr_hour = read(files[i],"start_hour")
-                curr_sec = read(files[i],"start_second")
-                curr_month = read(files[i],"start_month")
-                curr_year = read(files[i],"start_year")
-                append!(start_day, curr_day)
-			    append!(start_minute, curr_min)
-			    append!(start_hour, curr_hour)
-			    append!(start_second, curr_sec)
-			    append!(start_month, curr_month)
-			    append!(start_year, curr_year)
-			    push!(data, read(files[i],"data"))
-            catch x
-                println("Unable to read file:", files[i])
-            end
+		data = Any[]
+		Fs = Vector{Float64}()
+		for i in eachindex(files)
+			if Dates.value(Date(read(files[i],"start_year")[1],read(files[i],"start_month")[1],read(files[i],"start_day")[1])) >= Dates.value(Date(first_date)) && Dates.value(Date(read(files[i],"start_year")[1],read(files[i],"start_month")[1],read(files[i],"start_day")[1])) <= Dates.value(Date(last_date))
+
+			#Need the [1] behind each "read" function because "read" returns an array instead of an Int64
+				
+				append!(start_day, read(files[i],"start_day"))
+				append!(start_minute, read(files[i],"start_minute"))
+				append!(start_hour, read(files[i],"start_hour"))
+				append!(start_second, read(files[i],"start_second"))
+				append!(start_month, read(files[i],"start_month"))
+				append!(start_year, read(files[i],"start_year"))
+				push!(data, read(files[i],"data"))
+				append!(Fs, read(files[i],"Fs"))
+			end
 		end
-		return start_year,start_month,start_day,start_hour,start_minute,start_second,data
-end
-
-function start_time(time_data::Tuple) #Get time series for each data set, push into array of arrays
-	times = Any[]
-	date_times = []
-	for i in 1:length(time_data[3])
-		push!(times, collect(Dates.value.(Time(time_data[4][i],time_data[5][i],time_data[6][i]))/1e9:Dates.value.(Time(time_data[4][i],time_data[5][i],time_data[6][i])+Dates.Second(length(time_data[7][i])-1))/1e9)/3600)
-		push!(date_times, Dates.value(Date(time_data[1][i],time_data[2][i],time_data[3][i])))
+		return start_year,start_month,start_day,start_hour,start_minute,start_second,data,Fs
 	end
-	return times,date_times
-end
 
-function merge_data(data_times::Tuple,time_data::Tuple,un_wrap::Bool) 
-    #Collect each day of data into their own array, then push array of full day of data into final_data. 
-    #Same with times for each day
-    final_data = Any[]
-    final_time = Any[]
-    years = []
-    months = []
-    copy_data = copy(time_data[7])
-    copy_start = unique(data_times[2]) #Unique Date-Time objects - covers the span
-    duration = copy(data_times[2]) #Date-Time objects associated with each input file
-    copy_time = copy(data_times[1])
-    day_data = Any[]
-    date_data = Any[]
-    for i in 1:length(copy_start) #cycle through unique days
-        empty!(day_data) #clear current day data array
-        empty!(date_data)#clear current day time array
-        for j in 1:length(duration) #cycle through unique days... should this be 'duration' instead?
-            #if j > length(copy_data)
-            #    break
-            #end
-            if copy_start[i]==duration[j]
-                append!(day_data, copy_data[j])
-                append!(date_data, copy_time[j])
+    function start_time(time_data::Tuple)
+		times = Any[]
+		date_times = []
+		for i in 1:length(time_data[3])
+			 push!(times, collect(Dates.value.(Time(time_data[4][i],time_data[5][i],time_data[6][i]))/1e9:1/time_data[8][i]:Dates.value.(Time(time_data[4][i],time_data[5][i],time_data[6][i])+Dates.Millisecond(length(time_data[7][i])*1000/time_data[8][i]-1))/1e9)/3600)
+
+			#for times, the increments are in milliseconds. We multiply the length of the data array by 1000/frequency to trick the dataset into thinking it is being sampled at 1Hz (This gives us the correct time duration for the data set). The time array has steps of 1/Frequency so there are enough "data points" every second.
+			
+			push!(date_times, Dates.value(Date(time_data[1][i],time_data[2][i],time_data[3][i]))) 
+			#Same date_times variable, nothing changed
+		end
+		return times,date_times
+	end
+
+    function merge_data(data_times::Tuple,time_data::Tuple,un_wrap::Bool) 
+        #Collect each day of data into their own array, then push array of full day of data into final_data. 
+        #Same with times for each day
+        final_data = Any[]
+        final_time = Any[]
+        years = []
+        months = []
+        copy_data = copy(time_data[7])
+        copy_start = unique(data_times[2]) #Unique Date-Time objects - covers the span
+        duration = copy(data_times[2]) #Date-Time objects associated with each input file
+        copy_time = copy(data_times[1])
+        day_data = Any[]
+        date_data = Any[]
+        for i in eachindex(copy_start) #cycle through unique days
+            empty!(day_data) #clear current day data array
+            empty!(date_data)#clear current day time array
+            for j in eachindex(duration) #cycle through unique days... should this be 'duration' instead?
+                #if j > length(copy_data)
+                #    break
+                #end
+                if copy_start[i]==duration[j]
+                    append!(day_data, copy_data[j])
+                    append!(date_data, copy_time[j])
+                end
+            end
+            if length(day_data) > 0
+                if un_wrap == true
+                    copy_day_data = deepcopy(day_data)
+                    copy_date_data = deepcopy(date_data)
+                    push!(final_time, copy_date_data)
+                    push!(final_data, unwrap(copy_day_data))
+                    push!(years, year(copy_start[i]))
+                    push!(months, month(copy_start[i]))
+                    #push!(years, time_data[1][i])
+                    #push!(months, time_data[2][i])
+                else
+                    copy_day_data = deepcopy(day_data)
+                    copy_date_data = deepcopy(date_data)
+                    push!(final_time, copy_date_data)
+                    push!(final_data, copy_day_data)
+                    push!(years, year(copy_start[i]))
+                    push!(months, month(copy_start[i]))
+                    #push!(years, time_data[1][i])
+                    #push!(months, time_data[2][i])
+                end
             end
         end
-        if length(day_data) > 0
-            if un_wrap == true
-                copy_day_data = deepcopy(day_data)
-                copy_date_data = deepcopy(date_data)
-                push!(final_time, copy_date_data)
-                push!(final_data, unwrap(copy_day_data))
-                push!(years, year(copy_start[i]))
-                push!(months, month(copy_start[i]))
-                #push!(years, time_data[1][i])
-                #push!(months, time_data[2][i])
-            else
-                copy_day_data = deepcopy(day_data)
-                copy_date_data = deepcopy(date_data)
-                push!(final_time, copy_date_data)
-                push!(final_data, copy_day_data)
-                push!(years, year(copy_start[i]))
-                push!(months, month(copy_start[i]))
-                #push!(years, time_data[1][i])
-                #push!(months, time_data[2][i])
+        return final_data,final_time,years,months
+    end
+
+    function label_maker(merged_data::Tuple,time_data::Tuple,number)
+        line_labels = []
+        unique_days = []
+            for n in 1:length(time_data[3])-1
+                if time_data[3][n+1]!=time_data[3][n]
+                    append!(unique_days, time_data[3][n])
+                end
             end
-        end
+            n=length(time_data[3])
+            m = length(unique_days)
+            if time_data[3][n]!=unique_days[m]
+                append!(unique_days, time_data[3][n])
+            end
+            for i in 1:length(merged_data[3])
+                push!(line_labels, Date(merged_data[3][i],merged_data[4][i],unique_days[i]))
+            end
+            final_line_labels = []
+            k = round(length(line_labels)/(number-1))
+            l = round((Dates.value(last(line_labels))-Dates.value(line_labels[1]))/(number-1))
+            j = collect(1:number)
+            h = 0
+            for i in eachindex(line_labels)
+                if i == 1 || i == length(line_labels)
+                    push!(final_line_labels, line_labels[i])
+                    h+=1
+                elseif (i == j[h]*k && i+5 < length(line_labels))
+                    push!(final_line_labels, Date(Dates.UTD(Dates.value(line_labels[1])+j[h]*l)))
+                    h+=1
+                else
+                    push!(final_line_labels, "")
+                end
+    
+            end
+        return final_line_labels,line_labels
     end
-    return final_data,final_time,years,months
-end
 
-function label_maker(merged_data::Tuple,time_data::Tuple)
-    line_labels = []
-    unique_days = []
-    for n in 1:length(time_data[3])-1
-        if time_data[3][n+1]!=time_data[3][n]
-            append!(unique_days, time_data[3][n])
-        end
-    end
-    if time_data[3][end]!=unique_days[end]
-        append!(unique_days, time_data[3][end])
-    end
-    for i in 1:length(merged_data[3])
-        push!(line_labels, Date(merged_data[3][i],merged_data[4][i],unique_days[i]))
-    end
-    return line_labels
-end
+    function plot_data(axis_data1::Tuple, line_labels::Tuple, xlabel1, plot_title)
+	    n = length(axis_data1[1])
+		base = Dates.value(line_labels[2][1]) #full value of first day
+	    endpoint = Dates.value(last(line_labels[2])) #full value of last day
+	
+	    fig, ax = subplots()
+	    cmap = matplotlib.cm.get_cmap("jet")
+		tick_positions = Vector{Float64}()
+		final_line_labels=[]
+	
+	    for i in 1:n
+			#Line_color can be a number from 0 to 1, and divides the current days "value" by the last days "value". So the first day will have a cmap index of 0, and the last day will have a cmap index of 1
+	        line_color = cmap((Dates.value(line_labels[2][i])-base) / (endpoint-base))
 
-function plot_data(axis_data1::Tuple, line_labels, xlabel1, plot_title)
-    n = length(axis_data1[1])
-
-    fig, ax = subplots()
-    cmap = matplotlib.cm.get_cmap("jet")
-
-    for i in 1:n
-        line_color = cmap((i - 1) / (n - 1))
-        ax.plot(axis_data1[2][i], axis_data1[1][i], color=line_color, 				label=line_labels[i], linewidth=0.5)
-    end
-    ax.grid(true)
-
-    norm = matplotlib.colors.Normalize(vmin=0, vmax=n - 1)
-    sm = matplotlib.cm.ScalarMappable(cmap=cmap, norm=norm)
-    sm.set_array([])
-    cbar = fig.colorbar(sm, ax=ax)
-    cbar.set_label("Date")
-    xticks(0:2:24)
-    ylabel(xlabel1)
-    #ylim((-180,180)) #set y-limits
-    xlabel("UTC Time (Hrs)")
-    title(plot_title)
-
-    # Set custom tick labels on the colorbar
-    tick_positions = range(0, stop=n-1, length=n)
-    cbar.set_ticks(tick_positions)
-    cbar.set_ticklabels(line_labels)
-
-    show()
-end
+			#Same principal as the cmap index, but the ending tick of the colorbar corresponds to how many data points are represented by the colorbar. So, multiply by (n-1) so the index goes from 0 to (# of days plotted) instead of 0 to 1.
+			if line_labels[1][i] != ""
+				append!(tick_positions, ((Dates.value(line_labels[1][i])-base) / (endpoint-base))*(n-1))
+				push!(final_line_labels, line_labels[1][i])
+	       		ax.plot(axis_data1[2][i], axis_data1[1][i], color=line_color, 				label=line_labels[1][i], linewidth=0.5)
+			else
+				ax.plot(axis_data1[2][i],axis_data1[1][i], color=line_color, label="", linewidth=0.5)
+			end
+	    end
+	    ax.grid(true)
+	
+	    norm = matplotlib.colors.Normalize(vmin=0, vmax=n - 1)
+	    sm = matplotlib.cm.ScalarMappable(cmap=cmap, norm=norm)
+	    sm.set_array([])
+	    cbar = fig.colorbar(sm, ax=ax)
+	    cbar.set_label("Date")
+		xticks(0:4:24)
+		ylabel(xlabel1)
+		xlabel("Time (Hrs)")
+		title(plot_title)
+	
+	    #set custom tick labels on the colorbar
+	    cbar.set_ticks(tick_positions)
+	    cbar.set_ticklabels(final_line_labels)
+	
+	    show()
+	end
 
 function unwrap(phase::Vector{Any})
-    unwrapped_phase = similar(phase)
+    unwrapped_phase = copy(phase)
     unwrapped_phase[1] = phase[1]
 
-    for i = 2:length(phase)
+    for i in 2:length(phase) #eachindex() doesn't work here and I don't know why
         diff = phase[i] - phase[i - 1]
         if diff > 180
             phase[i] = phase[i] - 360 * ceil((diff - 180) / 360)
@@ -193,5 +221,11 @@ function unwrap(phase::Vector{Any})
 
     return phase
 end
-
+    
 end
+b=read_multiple_mat_files("C:\\Users\\1ryan\\OneDrive - UCB-O365\\ARL Data","NLK_NS_B")
+time_data2 = read_data(b,"2023-04-01","2023-07-17")
+data_times2 = start_time(time_data2)
+merged_data2 = merge_data(data_times2,time_data2,true)
+line_label2 = label_maker(merged_data2,time_data2,5)
+plot_data(merged_data2, line_label2, "Phase Shift","ARL")
